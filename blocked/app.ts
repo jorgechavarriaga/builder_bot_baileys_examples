5a7ec4f0fdc8204f6982896f2bbe36df738400a5
Jorge Chavarriaga <jorge.chavarriaga@codeboxx.biz>
2024-04-02 11:15:54 -0400

    blocked users cleaning code

--- blocked/app.ts
+++ blocked/app.ts
@@ -1,16 +1,15 @@
-import { createBot, createProvider, createFlow, addKeyword, utils, EVENTS } from '@builderbot/bot'
+import { createBot, createProvider, createFlow, addKeyword, EVENTS } from '@builderbot/bot'
 import { MemoryDB as Database } from '@builderbot/bot'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 import { config } from 'dotenv'
 config()
 
 const PHONE_NUMBER = process.env.PHONE_NUMBER
-const PORT = process.env.PORT ?? 3008
 
 const welcomeFlow = addKeyword<Provider, Database>(EVENTS.WELCOME)
-    .addAnswer(`🙌 Example Test`)
+    .addAnswer(`💡 Example *List Blocked Users on BOT:*`)
     .addAction(
-        async (ctx, { provider, flowDynamic }) => {
+        async (_, { provider, flowDynamic }) => {
             const blocked = await provider.vendor.fetchBlocklist()
             const result = blocked.map((id, index) => `id ${index + 1}: ${id}`).join('\n');
             await flowDynamic(`*Blocked:*\n${result}`)
@@ -23,45 +22,12 @@ const main = async () => {
     const adapterProvider = createProvider(Provider, { usePairingCode: true, phoneNumber: PHONE_NUMBER })
     const adapterDB = new Database()
 
-    const { handleCtx, httpServer } = await createBot(
+    const botResult = await createBot(
         {
             flow: adapterFlow,
             provider: adapterProvider,
             database: adapterDB,
         }
-    )
-
-
-    httpServer(+PORT)
-
-    adapterProvider.http.server.post(
-        '/v1/messages',
-        handleCtx(async (bot, req, res) => {
-            const { number, message, urlMedia } = req.body
-            await bot.sendMessage(number, message, { media: urlMedia ?? null })
-            return res.end('sended')
-        })
-    )
-
-    adapterProvider.http.server.post(
-        '/v1/register',
-        handleCtx(async (bot, req, res) => {
-            const { number, name } = req.body
-            await bot.dispatch('REGISTER_FLOW', { from: number, name })
-            return res.end('trigger')
-        })
-    )
-
-    adapterProvider.http.server.post(
-        '/v1/blacklist',
-        handleCtx(async (bot, req, res) => {
-            const { number, intent } = req.body
-            if (intent === 'remove') bot.blacklist.remove(number)
-            if (intent === 'add') bot.blacklist.add(number)
-
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            return res.end(JSON.stringify({ status: 'ok', number, intent }))
-        })
     )
 }
 
