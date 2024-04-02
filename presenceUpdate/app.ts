f5800d2c5bade7977d5f9ac72cf33f5bca280e40
Jorge Chavarriaga <jorge.chavarriaga@codeboxx.biz>
2024-04-02 17:55:13 -0400

    clean presenceUpdate add some files to gitignore

--- presenceUpdate/app.ts
+++ presenceUpdate/app.ts
@@ -1,11 +1,10 @@
-import { createBot, createProvider, createFlow, addKeyword, utils, EVENTS } from '@builderbot/bot'
+import { createBot, createProvider, createFlow, addKeyword, EVENTS } from '@builderbot/bot'
 import { MemoryDB as Database } from '@builderbot/bot'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 import { config } from 'dotenv'
 config()
 
 const PHONE_NUMBER = process.env.PHONE_NUMBER
-const PORT = process.env.PORT ?? 3008
 
 const waitT = (ms: number) => {
     return new Promise((resolve) => {
@@ -16,13 +15,13 @@ const waitT = (ms: number) => {
 }
 
 const welcomeFlow = addKeyword<Provider, Database>(EVENTS.WELCOME)
-    .addAnswer(`🙌 Example send Presence Update`)
+    .addAnswer(`💡 Example *Sending Presence Update*`)
     .addAction(
         async (ctx, { provider, flowDynamic }) => {
             await flowDynamic('This is an example of presence update')
-            await provider.vendor.sendPresenceUpdate('composing', ctx.key.remoteJid)
-            await waitT(5000)
             await provider.vendor.sendPresenceUpdate('recording', ctx.key.remoteJid)
+            await waitT(5000)
+            await provider.vendor.sendPresenceUpdate('composing', ctx.key.remoteJid)
             await waitT(5000)
             await flowDynamic('Great!')
         }
@@ -30,11 +29,9 @@ const welcomeFlow = addKeyword<Provider, Database>(EVENTS.WELCOME)
 
 const main = async () => {
     const adapterFlow = createFlow([welcomeFlow])
-
     const adapterProvider = createProvider(Provider, { usePairingCode: true, phoneNumber: PHONE_NUMBER })
     const adapterDB = new Database()
-
-    const { handleCtx, httpServer } = await createBot(
+    const botResult = await createBot(
         {
             flow: adapterFlow,
             provider: adapterProvider,
@@ -42,38 +39,6 @@ const main = async () => {
         }
     )
 
-
-    httpServer(+PORT)
-
-    adapterProvider.http.server.post(
-        '/v1/messages',
-        handleCtx(async (bot, req, res) => {
-            const { number, message, urlMedia } = req.body
-            await bot.sendMessage(number, message, { media: urlMedia ?? null })
-            return res.end('sended')
-        })
-    )
-
-    adapterProvider.http.server.post(
-        '/v1/register',
-        handleCtx(async (bot, req, res) => {
-            const { number, name } = req.body
-            await bot.dispatch('REGISTER_FLOW', { from: number, name })
-            return res.end('trigger')
-        })
-    )
-
-    adapterProvider.http.server.post(
-        '/v1/blacklist',
-        handleCtx(async (bot, req, res) => {
-            const { number, intent } = req.body
-            if (intent === 'remove') bot.blacklist.remove(number)
-            if (intent === 'add') bot.blacklist.add(number)
-
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            return res.end(JSON.stringify({ status: 'ok', number, intent }))
-        })
-    )
 }
 
 main()
