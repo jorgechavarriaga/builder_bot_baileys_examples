c2c0ad79166905c659ba27c8ddff8b1322d5c906
Jorge Chavarriaga <jorge.chavarriaga@codeboxx.biz>
2024-04-03 09:09:53 -0400

    delete user message cleaning code

--- deleteUserMessage/app.ts
+++ deleteUserMessage/app.ts
@@ -1,11 +1,10 @@
-import { createBot, createProvider, createFlow, addKeyword, utils, EVENTS } from '@builderbot/bot'
-import { MemoryDB as Database } from '@builderbot/bot'
+import { createBot, createProvider, createFlow, addKeyword, EVENTS } from '@builderbot/bot'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
+import { MemoryDB as Database } from '@builderbot/bot'
 import { config } from 'dotenv'
 config()
 
 const PHONE_NUMBER = process.env.PHONE_NUMBER
-const PORT = process.env.PORT ?? 3008
 
 const badWords = ['fuck', 'ass hole', 'motherfucker']
 
@@ -18,7 +17,7 @@ const waitT = (ms: number) => {
 }
 
 const welcomeFlow = addKeyword<Provider, Database>(EVENTS.WELCOME)
-    .addAnswer(`🙌 Example Delete User Message:`)
+    .addAnswer(`💡 Example *Delete User Message:*`)
     .addAction(
         async (ctx, { provider, flowDynamic }) => {
             const resp = ctx.body.toLocaleLowerCase()
@@ -45,49 +44,14 @@ const welcomeFlow = addKeyword<Provider, Database>(EVENTS.WELCOME)
 
 const main = async () => {
     const adapterFlow = createFlow([welcomeFlow])
-
     const adapterProvider = createProvider(Provider, { usePairingCode: true, phoneNumber: PHONE_NUMBER })
     const adapterDB = new Database()
-
-    const { handleCtx, httpServer } = await createBot(
+    const botResult = await createBot(
         {
             flow: adapterFlow,
             provider: adapterProvider,
             database: adapterDB,
         }
-    )
-
-
-    httpServer(+PORT)
-
-    adapterProvider.http.server.post(
-        '/v1/messages',
-        handleCtx(async (bot, req, res) => {
-            const { number, message, urlMedia } = req.body
-            await bot.sendMessage(number, message, { media: urlMedia ?? null })
-            return res.end('sended')
-        })
-    )
-
-    adapterProvider.http.server.post(
-        '/v1/register',
-        handleCtx(async (bot, req, res) => {
-            const { number, name } = req.body
-            await bot.dispatch('REGISTER_FLOW', { from: number, name })
-            return res.end('trigger')
-        })
-    )
-
-    adapterProvider.http.server.post(
-        '/v1/blacklist',
-        handleCtx(async (bot, req, res) => {
-            const { number, intent } = req.body
-            if (intent === 'remove') bot.blacklist.remove(number)
-            if (intent === 'add') bot.blacklist.add(number)
-
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            return res.end(JSON.stringify({ status: 'ok', number, intent }))
-        })
     )
 }
 
