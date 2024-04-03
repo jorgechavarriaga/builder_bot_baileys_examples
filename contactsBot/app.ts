02e40d7d95b353900cb50eae8ff2775c0e9b23ff
Jorge Chavarriaga <jorge.chavarriaga@codeboxx.biz>
2024-04-03 12:58:35 -0400

    contacts on bot code cleaned

--- contactsBot/app.ts
+++ contactsBot/app.ts
@@ -1,26 +1,23 @@
-import { createBot, createProvider, createFlow, addKeyword, utils, EVENTS } from '@builderbot/bot'
-import { MemoryDB as Database } from '@builderbot/bot'
+import { createBot, createProvider, createFlow, addKeyword, EVENTS } from '@builderbot/bot'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
+import { MemoryDB as Database } from '@builderbot/bot'
 import { config } from 'dotenv'
 config()
 
 const PHONE_NUMBER = process.env.PHONE_NUMBER
-const PORT = process.env.PORT ?? 3008
 let contacts = {}
-const xx = []
 
 const welcomeFlow = addKeyword<Provider, Database>(EVENTS.WELCOME)
-    .addAnswer(`🙌 Example Contacts from BOT`)
-    .addAnswer(
-        '*Random 5 contacts:* ', null,
+    .addAnswer(`💡 Example *Contacts on BOT*`)
+    .addAction(
         async (_, { flowDynamic }) => {
             const contactsBot = Object.entries(contacts)
                 .filter(([id, _]) => id.includes('@s.whatsapp.net'))
                 .map(([id, data]) => ({ id, name: data }))
-            const contactsBotRandom = contactsBot.sort(() => Math.random() - 0.5).slice(0, 5)
+            await flowDynamic(`Total contacts: ${contactsBot.length}`)
             const messages: string[] = []
-            for (const grupo of contactsBotRandom) {
-                const message = `${grupo.name ? '*Object:*' : ''} ${JSON.stringify(grupo?.name, null, 5) || ''}\n*Object:* ${grupo.id}`
+            for (const grupo of contactsBot) {
+                const message = `${grupo.name ? 'Object:' : ''} ${JSON.stringify(grupo?.name, null, 5) || ''}\n*id:* ${grupo.id}`
                 messages.push(message);
             }
             const concatenatedMessages = messages.join('\n\n')
@@ -44,6 +41,7 @@ const main = async () => {
 
     try {
         adapterProvider.on("ready", () => {
+            adapterProvider.store
             if (adapterProvider.store && adapterProvider.store.contacts) {
                 contacts = adapterProvider.store.contacts
             }
@@ -51,38 +49,6 @@ const main = async () => {
     } catch (error) {
         console.log(error)
     }
-
-    httpServer(+PORT)
-
-    adapterProvider.http.server.post(
-        '/v1/messages',
-        handleCtx(async (bot, req, res) => {
-            const { number, message, urlMedia } = req.body
-            await bot.sendMessage(number, message, { media: urlMedia ?? null })
-            return res.end('sended')
-        })
-    )
-
-    adapterProvider.http.server.post(
-        '/v1/register',
-        handleCtx(async (bot, req, res) => {
-            const { number, name } = req.body
-            await bot.dispatch('REGISTER_FLOW', { from: number, name })
-            return res.end('trigger')
-        })
-    )
-
-    adapterProvider.http.server.post(
-        '/v1/blacklist',
-        handleCtx(async (bot, req, res) => {
-            const { number, intent } = req.body
-            if (intent === 'remove') bot.blacklist.remove(number)
-            if (intent === 'add') bot.blacklist.add(number)
-
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            return res.end(JSON.stringify({ status: 'ok', number, intent }))
-        })
-    )
 }
 
 main()
